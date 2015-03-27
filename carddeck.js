c99615dfc2f1f6bbfc79671947950fa29eb1c743
Nick Stares <Nick Stares>
2015-03-27 14:18:09 -0700

    inital commit

---
+++ carddeck.js
@@ -0,0 +1,48 @@
+function Card(suit, rank) {
+  this.suit = suit
+  this.rank = rank
+}
+
+Card.SUITS = [
+  Card.CLUBS    = 'Clubs',
+  Card.DIAMONDS = 'Diamonds',
+  Card.HEARTS   = 'Hearts',
+  Card.SPADES   = 'Spades'
+]
+
+Card.prototype = {
+  get face_card() {
+    return this.rank > 10
+  },
+  valueOf: function() {
+    return this.rank
+  },
+  toString: function() {
+    var rank = {1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King'}[this.rank] || this.rank
+    return rank + ' of ' + this.suit
+  }
+}
+
+function Deck() {
+  this.cards = []
+  
+  for (var i = 0; i < Card.SUITS.length; i++) {
+    for (var rank = 1; rank <= 13; rank++) {
+      this.cards.push(new Card(Card.SUITS[i], rank))
+    }
+  }
+  
+  this.shuffle()
+}
+
+Deck.prototype = {
+  count: function() {
+    return this.cards.length
+  },
+  draw: function(n) {
+    return this.cards.splice(-n, n)
+  },
+  shuffle: function() {
+    this.cards.sort(function() { return Math.random() - 0.5 })
+  }
+};
