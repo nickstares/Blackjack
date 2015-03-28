32a042659f10a09a067983c531e76da1915ca5cf
Nick Stares <Nick Stares>
2015-03-28 10:21:20 -0700

    some game logic complete

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var redis = require("redis");
+var client = redis.createClient();
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+
+
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+//middleware below
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
+
+var loggedIn = [];
+
+// Root Route && Login
+app.get('/', function(req, res) {
+ res.render('index');
+});
+
+// Render new user page
+app.get('/newUser', function(req, res){
+ res.render('newUser');
+});
+
+// Enter global chat
+app.get('/globalchat', function(req, res){
+  res.render('globalchat');
+});
+
+// Create new User
+ //validate uniqueness of userName
+ app.post("/newuser", function(req, res){
+   client.HSETNX("users", req.body.userName, req.body.userPass, function(err, success) {
+     if (success === 1) {
+       res.redirect('/');
+     } else {
+       console.log("person already exists, figure out how to render this to the page");
+     }
+   });
+ });
+
+
+
+//validates userPass === userName and logs in
+ app.post("/globalchat", function(req, res){
+  var getUserPass = function(){
+    client.HGET("users", req.body.userName, function(err, reply){
+      if (err){
+        console.log("Could not query the database");
+      }
+
+      if (req.body.userPass == reply){
+        res.redirect("/globalchat");
+      } else {
+        console.log("Incorrect UserName or Password");
+        res.redirect('/');
+      }
+    });
+  };
+  getUserPass();
+});
+
+
+var usersLoggedIn = [];
+
+
+
+//start the server
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
--- carddeck.js
+++ carddeck.js
@@ -1,4 +1,4 @@
-function Card(suit, rank) {
+var Card = function (suit, rank) {
   this.suit = suit
   this.rank = rank
 }
@@ -23,17 +23,19 @@ Card.prototype = {
   }
 }
 
-function Deck() {
+
+var Deck = function() {
   this.cards = []
-  
-  for (var i = 0; i < Card.SUITS.length; i++) {
+  for (var i = 0; i < Card.SUITS.length; i++) {   
     for (var rank = 1; rank <= 13; rank++) {
       this.cards.push(new Card(Card.SUITS[i], rank))
     }
   }
-  
   this.shuffle()
 }
+
+// gameState = {currentTurn: 2, players:[{name: "dealer"} {name: "nick", faceUp: [instances], faceDown: [instances], }]}
+
 
 Deck.prototype = {
   count: function() {
@@ -43,6 +45,79 @@ Deck.prototype = {
     return this.cards.splice(-n, n)
   },
   shuffle: function() {
-    this.cards.sort(function() { return Math.random() - 0.5 })
+    this.cards.sort(function() { return Math.random() - 0.5 })   
   }
 };
+
+
+var Player = function(name){
+    this.name = name;
+    this.money = 100;
+    this.hand = [];
+}
+
+
+// GAME CLASS + PROTOTYPE
+
+var Game = function(name) {
+    this.playersArray = []
+    this.playersArray.push(new Player("Dealer"));
+    this.playersArray.push(new Player(name));
+    this.currentDeck = new Deck
+};
+
+Game.prototype.play = function() {
+
+}
+
+Game.prototype.deal = function(person, cards){ 
+    // this sets the current hand equal to the hand concatinated with the cards drawn
+    this.playersArray[person].hand = this.playersArray[person].hand.concat(this.currentDeck.draw(cards));
+}
+ 
+
+Game.prototype.checkForBlackjack = function() {
+     
+     cardDealer0 = this.playersArray[0].hand[0];
+     cardDealer1 = this.playersArray[0].hand[1];
+
+     for(var i = 1; i <= this.playersArray.length -1; i++){
+     
+        card0 = this.playersArray[i].hand[0];
+        card1 = this.playersArray[i].hand[1];
+        if ((card0 > 10 && card1.rank === 1) || (card1>10 && card0.rank === 1)){
+            if((cardDealer0 > 10 && cardDealer1.rank === 1) || (cardDealer1 > 10 && cardDealer0.rank === 1)){
+                return true;
+            }else{
+       
+                return false;
+            }
+        }   
+     } 
+}
+
+Game.prototype.gameWon = function() {
+this.checkForBlackjack();
+//   this.checkForHigherValue();
+//   this.checkForDealerBust();   
+  
+}
+
+
+// ---------------------------------------
+
+var makeANewGame = function(){
+    
+    var g = new Game
+   console.log(g)
+    g.deal(0,2);
+   
+    g.deal(1,2);
+    
+    g.checkForBlackjack();
+
+}
+
+  
+
+makeANewGame();
